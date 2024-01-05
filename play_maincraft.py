59347d29716fb174b7cb77e72865cb9c3d612701
Efe <efekochak2017@gmail.com>
2024-01-05 22:02:48 +0300

    first

---
+++ play_maincraft.py
@@ -0,0 +1,37 @@
+import pygame
+from settings import level_map, height, weight
+from level import Level
+from Wires import wires
+
+
+def play_game():
+    #wires()
+    pygame.init()
+    screen = pygame.display.set_mode((weight, height))
+    clock = pygame.time.Clock()
+    fps = 60
+    count = 0
+    level = Level(level_map, screen, fps)
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_g:
+                    level = Level(level_map, screen, fps)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if count % 2 == 0:
+                    level.flag = True
+                else:
+                    level.flag = False
+                count += 1
+        screen.fill('black')
+        level.run()
+        if level.stop_play_maincraft:
+            running = False
+        pygame.display.update()
+        clock.tick(fps)
+
+play_game()
---
+++ run_game.py
@@ -0,0 +1,26 @@
+import pygame
+from head import play_game
+from Wires import wires
+
+
+pygame.init()
+weight, height = 1200, 600
+screen = pygame.display.set_mode((weight, height))
+background_image = pygame.image.load('data/back.png')
+screen.blit(background_image, (0, 0))
+clock = pygame.time.Clock()
+maincraft = True
+fps = 60
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if 74 <= event.pos[0] <= 198 and 38 <= event.pos[1] <= 159 and maincraft:
+                play_game()
+                screen = pygame.display.set_mode((weight, height))
+                background_image = pygame.image.load('data/back.png')
+                screen.blit(background_image, (0, 0))
+        pygame.display.update()
+        clock.tick(fps)
